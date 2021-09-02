cbc8dc6b6863fccc94dae668c606ec85d9d7acf3
phu024 <phuwadon.dev@gmail.com>
2021-09-02 22:16:51 +0700

    commit

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,12 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
 export class ParentComponent implements OnInit{
   parentData!: number;
   constructor(){}
 
   ngOnInit(): void {
-    this.parentData=0
+    this.parentData=0;
   }
 
   onClick2Child(){
     this.parentData +=1;
   }
+
 }
